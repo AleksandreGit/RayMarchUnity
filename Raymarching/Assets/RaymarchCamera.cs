ad126bee30bf71c03b6e87d5f1c5d76b8db1e51b
Alexandre <achapin@insa-rennes.fr>
2020-11-19 14:42:31 +0100

    Add raymarching + a sphere

--- Raymarching/Assets/RaymarchCamera.cs
+++ Raymarching/Assets/RaymarchCamera.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))] // We have to have a camera component
-[ExecuteInEditMode] // execute in edit mode, so that the raymarch shader is always updated on camera
+[ExecuteInEditMode, ImageEffectAllowedInSceneView] // execute in edit mode, so that the raymarch shader is always updated on camera
 public class RaymarchCamera : MonoBehaviour
 {
     /* SHADER */
     // We need to have a reference to our shader
-    [SerializeField] // to show it into the inspector
+    // That's why we get it through the instructor with [SerializeField]
+    [SerializeField] 
     private Shader _shader;
 
-    // We have to create a material linked to the shader
+    /* MATERIAL */
+    private Material _rayMarchMat;
+
     public Material _rayMarchMaterial {
         get{
-            if(_rayMarchMat && _shader){
+            if(!_rayMarchMat && _shader){
                 _rayMarchMat = new Material(_shader);
                 _rayMarchMat.hideFlags = HideFlags.HideAndDontSave;
             }
@@ -22,9 +25,9 @@ public class RaymarchCamera : MonoBehaviour
         }
     }
 
-    private Material _rayMarchMat;
-
     /* CAMERA */
+    private Camera _cam;
+
     public Camera _camera {
         get {
             if(!_cam){
@@ -33,9 +36,19 @@ public class RaymarchCamera : MonoBehaviour
             return _cam;
         }
     }
-    private Camera _cam;
+
+    public float _maxDistance;
+    public Vector4 _sphere1;
 
     /* RENDERING */
+    /* link to the documentation : https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnRenderImage.html */
+    /* Appelé après chaque rendu.
+     * Permet de modifier l'imga finale en lui appliquant des filtres de base.
+     * L'image en entrée est src, l'image en sortie après process est dest.
+     * On doit toujours soit utiliser Graphics.Blit(...) ou rendre un quad en 
+     * fullscreen si on override cette méthode.
+     * 
+     */
     private void OnRenderImage(RenderTexture src, RenderTexture dest) {
         if(!_rayMarchMaterial) {
             Graphics.Blit(src, dest);
@@ -45,7 +58,8 @@ public class RaymarchCamera : MonoBehaviour
         // We assign the entrant elements to our shader
         _rayMarchMaterial.SetMatrix("_CamFrustum", CamFrustum(_camera));
         _rayMarchMaterial.SetMatrix("_CamToWorld", _camera.cameraToWorldMatrix);
-        _rayMarchMaterial.SetVector("_CamWorldSpace", _camera.transform.position);
+        _rayMarchMaterial.SetFloat("_maxDistance", _maxDistance);
+        _rayMarchMat.SetVector("_sphere1", _sphere1);
 
         RenderTexture.active = dest; // We now draw the quad on which we will draw the output of the shader
         GL.PushMatrix();
@@ -61,25 +75,31 @@ public class RaymarchCamera : MonoBehaviour
         GL.MultiTexCoord2(0, 1.0f, 0.0f);
         GL.Vertex3(1.0f, 0.0f, 2.0f);
 
-        //TP
+        //TR
         GL.MultiTexCoord2(0, 1.0f, 1.0f);
-        GL.Vertex3(1.0f, 0.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 1.0f);
 
         //TL
-        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
         GL.Vertex3(0.0f, 1.0f, 0.0f);
 
         GL.End();
         GL.PopMatrix();
+
+        Debug.Log("OK");
     }
 
+    // Calcul de la matrice représentant le frustum
     private Matrix4x4 CamFrustum(Camera cam) {
         Matrix4x4 frustum = Matrix4x4.identity; // Matrice identité
+        // Calcul du champ de vision
         float fov = Mathf.Tan((cam.fieldOfView*0.5f) * Mathf.Deg2Rad);
 
+        // Caclul des vecteurs direction de la caméra
         Vector3 goUp = Vector3.up * fov;
         Vector3 goRight = Vector3.right * fov * cam.aspect;
 
+        // Définition des points du quad
         Vector3 TL = (-Vector3.forward - goRight + goUp); // top-left corner
         Vector3 TR = (-Vector3.forward + goRight + goUp); // top-right corner
         Vector3 BR = (-Vector3.forward + goRight - goUp); // top-left corner
--- Raymarching/Assets/Raymarching.shader
+++ Raymarching/Assets/Raymarching.shader
@@ -19,8 +19,9 @@
             #include "UnityCG.cginc"
 
             sampler2D _MainTex;
-            uniform float4 _CamWorldSpace;
             uniform float4x4 _CamFrustum, _CamToWorld;
+            uniform float _maxDistance;
+            uniform float4 _sphere1;
 
             struct appdata
             {
@@ -34,6 +35,43 @@
                 float4 vertex : SV_POSITION;
                 float3 ray : TEXCOORD1;
             };
+
+            float sdfSphere(float3 position, float radius) {
+                return length(position) - radius;
+            }
+
+            float distanceField(float3 position) {
+                float sphere1 = sdfSphere(position - _sphere1.xyz, _sphere1.w);
+                return sphere1;
+            }
+
+            fixed4 raymarching(float3 rayOrigin, float3 rayDirection) {
+                fixed4 result = fixed4(1, 1, 1, 1);
+
+                const int maxIteration = 64;
+                float distanceTravelled = 0; // distance travelled along the ray direction
+
+                // We loop through the iterations
+                for (int i = 0; i < maxIteration; i++) {
+                    if (distanceTravelled > _maxDistance) {
+                        // Envrionment color
+                        result = fixed4(rayDirection, 1);
+                        break;
+                    }
+
+                    float3 position = rayOrigin + rayDirection * distanceTravelled;
+                    // Check for hit in distanceField
+                    float distance = distanceField(position); // si < 0 dans objet, sinon extérieur
+                    if (distance < 0.01) { // We have hit something
+                        // Shading
+                        result = fixed4(1, 1, 1, 1);
+                        break;
+                    }
+                    distanceTravelled += distance;
+                }
+
+                return result;
+            }
 
             v2f vert (appdata v)
             {
@@ -55,9 +93,9 @@
             fixed4 frag(v2f i) : SV_Target
             {
                 float3 rayDirection = normalize(i.ray.xyz);
-                float3 rayOrigin = _CamWorldSpace;
-
-                return fixed4(rayDirection, 1);
+                float3 rayOrigin = _WorldSpaceCameraPos;
+                fixed4 result = raymarching(rayOrigin, rayDirection);
+                return result;
             }
             ENDCG
         }
